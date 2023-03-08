8426af99b9ec5b77e3fa202e9e0086b9ac2c4aa7
Jay Bui <jbui@Jays-MacBook-Pro.local>
2023-03-07 21:32:12 -0500

    modified ContactMe

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,19 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
+import githubLogo from "./GitHub-Logo.svg";
+import linkedinLogo from "./LinkedIn-Logo.svg";
+import gmailLogo from "./Gmail-Logo.svg";
+import twitterLogo from "./Twitter-Logo.svg";
+import instagramLogo from "./Instagram-Logo.svg";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // send the form data to the server or API here
+    console.log({ name, email, message });
+    // clear the form after submission
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <section className="contact">
-      <div className="contact-text-all">
-        <div className="contact-header">
-          <span className="logo-signs">&lt; </span>
-          <h1 className="contact-text-header">Find Me On .</h1>
-          <span className="logo-signs"> /&gt;</span>
+      <div className="contact-header">
+        <span className="logo-signs">&lt; </span>
+        <h1 className="contact-text-header">Let's Chat.</h1>
+        <span className="logo-signs"> /&gt;</span>
+      </div>
+      <div className="contact-body">
+        <div className="contact-text-all">
+          <h2 className="contact-text-subheader">Tell me about your ideas.</h2>
+          <p className="contact-text-p">We'll create something together ✌🏻</p>
+          <div className="logo-social-media">
+            <ul class="social-links">
+              <li>
+                <a href="https://github.com/">
+                  <img
+                    src={githubLogo}
+                    alt="Github Logo"
+                    width="32"
+                    height="32"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/">
+                  <img
+                    src={linkedinLogo}
+                    alt="LinkedIn Logo"
+                    width="32"
+                    height="32"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="mailto:youremailaddress@example.com">
+                  <img
+                    src={gmailLogo}
+                    alt="Gmail Logo"
+                    width="32"
+                    height="32"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/">
+                  <img
+                    src={twitterLogo}
+                    alt="Twitter Logo"
+                    width="32"
+                    height="32"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/">
+                  <img
+                    src={instagramLogo}
+                    alt="Instagram Logo"
+                    width="32"
+                    height="32"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <span className="contact-text-subheader">
-          WANT TO CREATE SOMETHING TOGETHER OR JUST SAY HI? MY INBOX IS ALWAYS
-          OPEN
-        </span>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </section>
   );
